04d98b328df14f5784da1c514d4573f4053529e5
Odaka Ui <31593320+odakaui@users.noreply.github.com>
2020-07-11 21:43:50 +0000

    Troubleshooting Travis Deploy

--- travis/before_deploy.sh
+++ travis/before_deploy.sh
@@ -20,6 +20,7 @@ cd $stage
 # if the commit has a tag add it to the filename
 if [ -z "$var" ]
 then
+    echo "Draft Release"
     tar czf "${src}/${NAME}-${TARGET}.tar.gz" *
 else
     tar czf "${src}/${NAME}-${TRAVIS_TAG}-${TARGET}.tar.gz" *

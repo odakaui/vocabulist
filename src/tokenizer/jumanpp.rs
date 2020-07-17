3d19ea744739498ae1dd3750bb8df20c09145631
Odaka Ui <31593320+odakaui@users.noreply.github.com>
2020-07-17 01:19:26 +0000

    Implement test for tokenize method

--- src/tokenizer/jumanpp.rs
+++ src/tokenizer/jumanpp.rs
@@ -4,7 +4,7 @@ use std::io::Write;
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
 
-/// send a sentence to jumanpp and return the result
+/// tokenize a sentence using the jumanpp binary at path
 fn jumanpp(path: &PathBuf, sentence: &str) -> Result<String, Box<dyn Error>> {
     let mut jumanpp = Command::new(path)
         .stdin(Stdio::piped())
@@ -24,6 +24,7 @@ fn jumanpp(path: &PathBuf, sentence: &str) -> Result<String, Box<dyn Error>> {
     Ok(token_string)
 }
 
+/// convert the output from jumanpp to a list of Token structs
 fn tokenize_output(sentence: &str, output: &str) -> Vec<Token> {
     let output_list: Vec<Vec<&str>> = output.lines().map(|x| x.split(' ').collect()).collect();
 
@@ -45,17 +46,20 @@ fn tokenize_output(sentence: &str, output: &str) -> Vec<Token> {
     token_list
 }
 
+/// the Jumanpp struct
 pub struct Jumanpp {
     path: PathBuf,
 }
 
 impl Jumanpp {
+    /// the Jumanpp constructor
     pub fn new(path: PathBuf) -> Self {
         Jumanpp { path }
     }
 }
 
 impl Tokenize for Jumanpp {
+    /// implement the required method for Tokenize
     fn tokenize(&self, sentence: &str) -> Result<Vec<Token>, Box<dyn Error>> {
         let token_string = jumanpp(&self.path, sentence)?;
         let token_list = tokenize_output(sentence, token_string.as_ref());
@@ -64,36 +68,12 @@ impl Tokenize for Jumanpp {
     }
 }
 
-// fn tokenize_sentence_list(
-//     sentence_list: &Vec<String>,
-//     callback: &dyn Fn(),
-// ) -> Vec<Token> {
-//     let mut token_list: Vec<Token> = Vec::new();
-//     for sentence in sentence_list.iter() {
-//         let e = tokenize_sentence(sentence);
-//         token_list.extend(e);
-//
-//         callback();
-//     }
-//
-//     token_list
-// }
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_tokenize_output() {
-        let sentence = "魅力がたっぷりと詰まっている";
-        let output = "魅力 みりょく 魅力 名詞 6 普通名詞 1 * 0 * 0 \"代表表記:魅力/みりょく カテゴリ:抽象物\"\n\
-            が が が 助詞 9 格助詞 1 * 0 * 0 NIL\n\
-            たっぷり たっぷり たっぷりだ 形容詞 3 * 0 ナノ形容詞 22 語幹 1 \"代表表記:たっぷりだ/たっぷりだ\"\n\
-            と と と 助詞 9 格助詞 1 * 0 * 0 NIL\n\
-            詰まって つまって 詰まる 動詞 2 * 0 子音動詞ラ行 10 タ系連用テ形 14 \"代表表記:詰まる/つまる ドメイン:料理・食事 自他動詞:他:詰める/つめる\"\n\
-            いる いる いる 接尾辞 14 動詞性接尾辞 7 母音動詞 1 基本形 2 \"代表表記:いる/いる\"\n\
-            EOS";
-
+    /// create the expected token list
+    fn expected_token_list() -> Vec<Token> {
         let mut expected_token_list: Vec<Token> = Vec::new();
         expected_token_list.push(
             Token::new("魅力".to_string())
@@ -132,63 +112,18 @@ mod tests {
                 .surface_string(vec!["いる".to_string()]),
         );
 
-        let token_list = tokenize_output(sentence, output);
-
-        assert_eq!(token_list, expected_token_list);
+        expected_token_list
     }
 
+    /// check the output of the tokenize method
     #[test]
-    #[should_panic]
-    fn test_tokenize_output_should_panic() {
-        let sentence = "Hello World";
-        let output = "魅力 みりょく 魅力 名詞 6 普通名詞 1 * 0 * 0 \"代表表記:魅力/みりょく カテゴリ:抽象物\"\n\
-            が が が 助詞 9 格助詞 1 * 0 * 0 NIL\n\
-            たっぷり たっぷり たっぷりだ 形容詞 3 * 0 ナノ形容詞 22 語幹 1 \"代表表記:たっぷりだ/たっぷりだ\"\n\
-            と と と 助詞 9 格助詞 1 * 0 * 0 NIL\n\
-            詰まって つまって 詰まる 動詞 2 * 0 子音動詞ラ行 10 タ系連用テ形 14 \"代表表記:詰まる/つまる ドメイン:料理・食事 自他動詞:他:詰める/つめる\"\n\
-            いる いる いる 接尾辞 14 動詞性接尾辞 7 母音動詞 1 基本形 2 \"代表表記:いる/いる\"\n\
-            EOS";
+    #[ignore]
+    fn test_tokenize() {
+        let sentence = "魅力がたっぷりと詰まっている";
+        let jumanpp = Jumanpp::new(PathBuf::from("jumanpp"));
 
-        let mut expected_token_list: Vec<Token> = Vec::new();
-        expected_token_list.push(
-            Token::new("魅力".to_string())
-                .pos(vec!["名詞".to_string()])
-                .sentence(vec!["魅力がたっぷりと詰まっている".to_string()])
-                .surface_string(vec!["魅力".to_string()]),
-        );
-        expected_token_list.push(
-            Token::new("が".to_string())
-                .pos(vec!["助詞".to_string()])
-                .sentence(vec!["魅力がたっぷりと詰まっている".to_string()])
-                .surface_string(vec!["が".to_string()]),
-        );
-        expected_token_list.push(
-            Token::new("たっぷりだ".to_string())
-                .pos(vec!["形容詞".to_string()])
-                .sentence(vec!["魅力がたっぷりと詰まっている".to_string()])
-                .surface_string(vec!["たっぷり".to_string()]),
-        );
-        expected_token_list.push(
-            Token::new("と".to_string())
-                .pos(vec!["助詞".to_string()])
-                .sentence(vec!["魅力がたっぷりと詰まっている".to_string()])
-                .surface_string(vec!["と".to_string()]),
-        );
-        expected_token_list.push(
-            Token::new("詰まる".to_string())
-                .pos(vec!["動詞".to_string()])
-                .sentence(vec!["魅力がたっぷりと詰まっている".to_string()])
-                .surface_string(vec!["詰まって".to_string()]),
-        );
-        expected_token_list.push(
-            Token::new("いる".to_string())
-                .pos(vec!["接尾辞".to_string()])
-                .sentence(vec!["魅力がたっぷりと詰まっている".to_string()])
-                .surface_string(vec!["いる".to_string()]),
-        );
+        let token_list = jumanpp.tokenize(sentence).expect("Failed to tokenize sentence");
 
-        let token_list = tokenize_output(sentence, output);
-
-        assert_eq!(token_list, expected_token_list);
+        assert_eq!(token_list, expected_token_list());
     }
 }
